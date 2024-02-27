2db9ecc1b993323d78ed748c791714cef430d8cc
Jimmy Givans <jauyoma.dev@gmail.com>
2024-02-27 15:52:18 +0300

    feat: create a schema for users

---
+++ src/server/db/User.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "./db.js";
+
+const User = sequelize.define("User", {
+  user_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  user_email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  user_password: {
+    type: DataTypes.STRING(64),
+    allowNull: false,
+  },
+  user_role: {
+    type: DataTypes.ENUM("Admin", "Regular"),
+    allowNull: false,
+  },
+});
+
+await User.sync({ force: true });
+console.log("The table for the User model was just created!");
+
+export default User;
