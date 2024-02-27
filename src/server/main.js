10ad707d7268f6f0a65a102fb7b1e3804700e9ff
Jimmy Givans <jauyoma.dev@gmail.com>
2024-02-27 15:55:43 +0300

    feat: import dotenv

--- src/server/main.js
+++ src/server/main.js
@@ -1,5 +1,6 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import "dotenv/config.js";
 
 const app = express();
 
@@ -7,6 +8,4 @@ app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
-);
+ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
