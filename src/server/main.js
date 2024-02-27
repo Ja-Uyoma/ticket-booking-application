5b3eb92c479caf1ac9dbc41fc2becf343a2dc59a
Jimmy Givans <jauyoma.dev@gmail.com>
2024-02-27 21:38:32 +0300

    feat: set up passport and express-session

--- src/server/main.js
+++ src/server/main.js
@@ -1,8 +1,16 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import "dotenv/config.js";
+import session from "express-session";
+import passport from "passport";
+import LocalStrategy from "passport-local";
 
 const app = express();
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
