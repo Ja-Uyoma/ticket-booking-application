d64f5f37e3fb29607ab27bc09b091b4b7b07d1ca
Jimmy Givans <jauyoma.dev@gmail.com>
2024-02-28 16:42:21 +0300

    feat: fix register post endpoint

--- src/server/main.js
+++ src/server/main.js
@@ -5,23 +5,35 @@ import session from "express-session";
 import passport from "passport";
 import LocalStrategy from "passport-local";
 import morgan from "morgan";
+import cors from "cors";
 import User from "./db/User.js";
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
 
 app.post("/api/v1/register", async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).send("Email cannot be empty");
+  } else if (!password) {
+    return res.status(400).send("Password cannot be empty");
+  }
+
   try {
-    await User.create({ user_email: req.body.email, user_password: req.body.password });
+    await User.create({ user_email: email, user_password: password });
     res.status(200).send("User registered successfully");
   } catch (err) {
-    return next(err);
+    console.log("User registration failed:", err);
+    next(err);
   }
 });
