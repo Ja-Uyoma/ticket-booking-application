22bbc8bbded35f149b035b6eca02023c030da0ab
Jimmy Givans <jauyoma.dev@gmail.com>
2024-02-28 08:24:50 +0300

    feat: delete hello route

--- src/server/main.js
+++ src/server/main.js
@@ -12,8 +12,4 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
-
 ViteExpress.listen(app, 3000, () => console.log("Server is listening on port 3000..."));
