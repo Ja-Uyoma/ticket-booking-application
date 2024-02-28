e8c322d45de923c7e00fbdffe16c00bbdb487a57
Jimmy Givans <jauyoma.dev@gmail.com>
2024-02-28 06:56:33 +0300

    feat: define a root route component

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -4,12 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./App";
+import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
 
