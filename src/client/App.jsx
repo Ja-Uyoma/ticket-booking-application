acbeb0f1423582bc51e3d4c888b22eccd9c343d9
Jimmy Givans <jauyoma.dev@gmail.com>
2024-02-29 21:56:05 +0300

    refactor: rewrite footer using daisyui component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -68,10 +68,27 @@ function SearchBar() {
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 py-6">
-      <p className="text-white text-center">
-        Copyright &copy; <span>2024</span> TicketPal. All rights reserved{" "}
-      </p>
+    <footer className="footer p-10 bg-neutral text-neutral-content">
+      <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
     </footer>
   );
 }
