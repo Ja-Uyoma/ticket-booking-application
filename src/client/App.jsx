9550fcc19283528bdf38fe5b5796ba657e6b7948
Jimmy Givans <jauyoma.dev@gmail.com>
2024-02-29 22:02:53 +0300

    refactor: rewrite header using daisyui

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -13,36 +12,38 @@ function App() {
 
 function Header() {
   return (
-    <div className="flex justify-around text-lg bg-blue-600 text-white p-4">
-      <header>
-        <h1 className="font-bold text-2xl italic">TicketPal</h1>
-      </header>
-      <div className="flex justify-between gap-2">
-        <div>
-          <a href="/" className="hover:cursor-pointer hover:bg-blue-900">
-            Concerts
-          </a>
-        </div>
-        <div>
-          <a href="/" className="hover:cursor-pointer hover:bg-blue-900">
-            Sports
-          </a>
-        </div>
-        <div>
-          <a href="/" className="hover:cursor-pointer hover:bg-blue-900">
-            Arts & Theater
-          </a>
-        </div>
-        <div>
-          <a href="/" className="hover:cursor-pointer hover:bg-blue-900">
-            Family
-          </a>
-        </div>
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">ticketpal</a>
       </div>
-      <div>
-        <Link to={`/login`} className="hover:cursor-pointer hover:bg-blue-900">
-          Sign In/Register
-        </Link>
+      <div className="flex-none gap-2">
+        <div className="form-control">
+          <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+        </div>
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </div>
+          <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
