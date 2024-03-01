e62416c01b16aa2ba9ea4e7000a88b26f862cced
Jimmy Givans <jauyoma.dev@gmail.com>
2024-03-01 09:41:49 +0300

    feat: render navbar and improve date formatting

--- src/client/routes/events.jsx
+++ src/client/routes/events.jsx
@@ -13,23 +13,52 @@ export default function Events() {
 
   return (
     <div>
-      {events.length ? (
-        events.map((event) => {
-          return (
-            <div className="card w-96 bg-base-100 shadow-xl" key={event.id}>
-              <div className="card-body">
-                <h2 className="card-title">{event.name}</h2>
-                <p>{event.description}</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Book Event</button>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+          </div>
+          <a href="/addEvent" className="btn btn-ghost text-xl">
+            Add Event
+          </a>
+        </div>
+        <div className="navbar-end">
+          <button className="btn">Logout</button>
+        </div>
+      </div>
+
+      {/**/}
+      <div>
+        {events.length ? (
+          events.map((event) => {
+            return (
+              <div className="card w-96 bg-base-100 shadow-xl" key={event.id}>
+                <div className="card-body">
+                  <h2 className="card-title">{event.name}</h2>
+                  <p>{event.description}</p>
+                  <p>{event.venue}</p>
+                  <p>{new Date(event.date).toLocaleString()}</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Book Event</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })
-      ) : (
-        <p>No events</p>
-      )}
+            );
+          })
+        ) : (
+          <p>No events</p>
+        )}
+      </div>
     </div>
   );
 }
